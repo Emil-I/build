0e1a20aae3a173996adbee25976eb87b98118bb0
_emil <emill.zuball@gmail.com>
2018-08-09 22:48:08 +0300

    add pages admin and index=landing(front)

---
+++ src/index/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to index"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/index/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,41 @@
+// DOC https://cli.vuejs.org/config/#pages
+
+
 module.exports = {
-    
-    baseUrl: process.env.NODE_ENV === 'production' ? '' : '/'
-    
+
+    baseUrl: process.env.NODE_ENV === 'production' ? '' : '/',
+
+    pages: {
+
+
+        index: {
+            // entry for the page
+            entry: 'src/main.ts',
+            // the source template
+            template: 'public/index.html',
+            // output as dist/index.html
+            filename: 'index.html',
+            // when using title option,
+            // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'Index Page',
+            // chunks to include on this page, by default includes
+            // extracted common chunks and vendor chunks.
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        admin: {
+            // entry for the page
+            entry: 'src/admin/main.ts',
+            // the source template
+            template: 'public/admin.html',
+            // output as dist/admin.html
+            filename: 'admin.html',
+            // when using title option,
+            // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'Admin Page',
+            // chunks to include on this page, by default includes
+            // extracted common chunks and vendor chunks.
+            chunks: ['chunk-vendors', 'chunk-common', 'admin']
+        }
+    }
+
 }
